f49b337f89738a2b414d2f0b8c4afbfa5b15ed8c
jokeri2222 <94902709+jokeri2222@users.noreply.github.com>
2024-02-05 02:56:39 +0200

    Create QuizletHack.meta.js

---
+++ QuizletHack.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Quizlet Hack
+// @version      1.0.0
+// @namespace    https://github.com/jokeri2222
+// @description  A hack for quizlet live
+// @updateURL    https://github.com/jokeri2222/Kahoot-Hack/raw/main/QuizletHack.meta.js
+// @downloadURL  https://github.com/jokeri2222/Kahoot-Hack/raw/main/QuizletHack.user.js
+// @author       jokeri2222; https://github.com/jokeri2222
+// @match        https://quizlet.com/live/*
+// @match        https://quizlet.com/live
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=quizlet.com
+// @grant        none
+// ==/UserScript==
