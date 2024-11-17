74d1d6050543f4983fdf628230fc223d019ee7f6
jokeri2222 <94902709+jokeri2222@users.noreply.github.com>
2024-11-17 17:16:08 +0200

    Update QuizletHack.user.js

--- QuizletHack.user.js
+++ QuizletHack.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Quizlet Hack
-// @version      1.0.4
+// @version      1.0.5
 // @namespace    https://github.com/jokeri2222
 // @description  A hack for quizlet live
 // @updateURL    https://github.com/jokeri2222/Quizlet-Hack/raw/main/QuizletHack.meta.js
